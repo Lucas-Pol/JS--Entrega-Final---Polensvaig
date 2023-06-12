8e3b89779a5e8400761df23f1780ecd47b1b0fd0
LucasPol <lucas.polensvaig@gmail.com>
2023-06-12 19:34:00 -0300

    functions updated

--- js/main.js
+++ js/main.js
@@ -1,141 +1,110 @@
-//setting variables
-let numberDisc = 0;
+// Setting variables
 let finalPrice = 0;
-let priceWithDiscount = 0;
-let cardText = document.getElementById("carro");
-let cardBtn = document.getElementById("cardbtn");
-
-
-//sales cart array
 let cart = [];
 
-
-//disc array
-
+// Disc array
 let cds = [
-    {
-        id: 1,
-        discName: "La cumbia del ciclo",
-        price: 100,
-    },
-    {
-        id: 2,
-        discName: "Binomio",
-        price: 200,
-    },
-    {
-        id: 3,
-        discName: "Ana María",
-        price: 300,
-    },
+  {
+    id: 1,
+    discName: "La cumbia del ciclo",
+    price: 100,
+  },
+  {
+    id: 2,
+    discName: "Binomio",
+    price: 200,
+  },
+  {
+    id: 3,
+    discName: "Ana María",
+    price: 300,
+  },
 ];
 
-
-
-//add to cart
-function addProd (id){ 
-    let priceCalc = 0;
-    for (let i = 0; i < cds.length; i++) {
-        // check the ID
-        if (cds[i].id === id) {
-         cart.push(cds[i]);
-         //calculate final price
-         priceCalc = finalPrice + cds[i].price;
-         finalPrice=priceCalc;
-        } 
-      }
-      console.log(finalPrice);
-      return cardText.textContent = `En el carrito hay: ${cart.length} discos 
-      por un valor de $ ${finalPrice}`; 
-}
-
-
-//checkout function
-function checkout (key, desc){
-    cardText.textContent = `En el carrito hay: ${cart.length} discos 
-                por un valor de $ ${key} y se aplicó un descuento del ${desc}`;
-    let nombre = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
-    let direccion = document.getElementById('direccion').value;
-    let email = document.getElementById('email').value;
-    console.log(`Estimado ${nombre} ${apellido}, el monto a pagar es de $ ${key}, se enviará la factura a ${mail}.`);
-    clearCart();
-}
-
-//discount function
-function discount (){
-    let discountCode = prompt ("Ingrese su código de descuento");
-           switch (discountCode) {
-            case "DESC10":
-                priceWithDiscount = finalPrice - (finalPrice * 0.1) 
-                checkout (priceWithDiscount, "10%");       
-                break;
-            case "DESC20":
-                priceWithDiscount = finalPrice - (finalPrice * 0.2); 
-                checkout (priceWithDiscount, "20%");    
-                break;
-            case "DESC30":
-                priceWithDiscount = finalPrice - (finalPrice * 0.3);
-                checkout (priceWithDiscount, "30%");    
-                break;
-            default:
-                console.log("No ingresaste ningún código o el ingresado no es válido");
-                checkout (finalPrice, "0%");
-                break;
-        }
-}
-
-
-
-//clear function
-function clearCart (){
-    numberDisc = 0;
-    finalPrice = 0;
-    priceWithDiscount = 0;
-    cart = [];
-    cardText.textContent = "Gracias por comprar";
-    cardBtn.innerHTML = `<a href="#" class="btn btn-success" onclick="">Compra Finalizada</a>`;
-}; 
-
-
-
-//purchase function 
-function realizarCompra() {
-    //this ir the form
-    let modal = document.getElementById('formularioCompra');
-    $('#formularioCompra').modal('show');
-    // Get the values from the form 
-    let nombre = document.getElementById('nombre').value;
-    let apellido = document.getElementById('apellido').value;
-    let direccion = document.getElementById('direccion').value;
-    let email = document.getElementById('email').value;
-  
-    // Create an object with the purchase data
-    let compra = {
-      nombre: nombre,
-      apellido: apellido,
-      direccion: direccion,
-      email: email,
-    };
-  
-    // Convert the object to JSON
-    let compraJSON = JSON.stringify(compra);
-  
-    // Save the JSON to Local Storage
-    localStorage.setItem('compra', compraJSON);
-  
-    // Display the JSON in the console
-    console.log(compraJSON);
+// Add to cart
+function addProd(id) {
+  const selectedDisc = cds.find(disc => disc.id === id);
+  if (selectedDisc) {
+    cart.push(selectedDisc);
+    finalPrice += selectedDisc.price;
+    updateCartText();
   }
-  
-
-//close modal function
-function cerrarModal(){
-    let modal = document.getElementById('formularioCompra');
-    $('#formularioCompra').modal('hide');
-    /* checkout(); */
 }
 
+// Update cart text
+function updateCartText() {
+  const cartText = document.getElementById("carro");
+  const itemCount = cart.length;
+  cartText.textContent = `En el carrito hay: ${itemCount} discos por un valor de $ ${finalPrice}`;
+}
 
+// Open purchase modal
+function openModal() {
+  $('#formularioCompra').modal('show');
+}
 
+// Purchase function
+function realizarCompra() {
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const direccion = document.getElementById('direccion').value;
+  const email = document.getElementById('email').value;
+
+  if (!nombre || !apellido || !direccion || !email) {
+    console.log("Por favor completa todos los campos del formulario.");
+    return;
+  }
+
+  const purchase = {
+    nombre: nombre,
+    apellido: apellido,
+    direccion: direccion,
+    email: email,
+    cart: cart,
+    total: finalPrice
+  };
+
+  const purchaseJSON = JSON.stringify(purchase);
+  localStorage.setItem('compra', purchaseJSON);
+
+  console.log(`Estimado ${nombre} ${apellido}, el monto a pagar es de $ ${finalPrice}, se enviará la factura a ${email}.`);
+
+  $('#formularioCompra').modal('hide');
+  discount();
+}
+
+// Clear cart
+function clearCart() {
+  finalPrice = 0;
+  cart = [];
+  updateCartText();
+}
+
+// Discount function
+function discount() {
+  const discountCode = prompt("Ingrese su código de descuento");
+  let priceWithDiscount = finalPrice;
+
+  switch (discountCode) {
+    case "DESC10":
+      priceWithDiscount *= 0.9;
+      break;
+    case "DESC20":
+      priceWithDiscount *= 0.8;
+      break;
+    case "DESC30":
+      priceWithDiscount *= 0.7;
+      break;
+    default:
+      console.log("No ingresaste ningún código o el ingresado no es válido");
+      break;
+  }
+
+  finalPrice = priceWithDiscount; // Actualizar el valor final con el descuento aplicado
+  updateCartText();
+  console.log(`El monto a pagar con descuento es de $ ${priceWithDiscount.toFixed(2)}`);
+}
+
+// Update cart text on page load
+updateCartText();
 
